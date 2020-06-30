096e191dfef6593eaef76b267b82f6510f07c590
supertailcat <hcyupy@outlook.com>
2020-06-30 17:01:21 +0800

    于沛尧：修改了该文件，改变了调用方式

--- GenerateJsonFile.py
+++ GenerateJsonFile.py
@@ -7,17 +7,16 @@ import csv
 import json
 
 
-class GenerateJsonFile:
+def csv_to_json(filename):
     csv_filename = ""
-
-    def __init__(self, filename):
-        csv_file = open(filename, 'r')
-        json_file = open('json_file.json', 'w')
-        fieldnames = ("DATE", "MAXT", "MINT", "AVGT")
-        reader = csv.DictReader(csv_file, fieldnames)
-        for row in reader:
-            json.dump(row, json_file)
-            json_file.write('\n')
+    csv_file = open(filename, 'r')
+    json_file = open('json_file.json', 'w')
+    fieldnames = ("DATE", "MAXT", "MINT", "AVGT")
+    reader = csv.DictReader(csv_file, fieldnames)
+    for row in reader:
+        json.dump(row, json_file)
+        json_file.write('\n')
 
 
-# gjf = GenerateJsonFile("maxmin.csv")
+if __name__ == "__main__":
+    csv_to_json("maxmin.csv")
