537127fcb60ce4245952ba9cc51b33d6ea9d2de1
master <root@master.com>
2020-07-07 01:07:17 +0800

    用flask重写websocket并实现初步通讯

--- Flask_demo/app.py
+++ Flask_demo/app.py
@@ -17,23 +17,25 @@ def check_permit(user_socket):
         user_socket.send(response_str)
 
 
-@app.route("/ws")
+@app.route("/ws", methods=['GET', 'POST'])
 def ws():
     user_socket = request.environ.get('wsgi.websocket')  # type:WebSocket
+    print("client connected!")
     if check_permit(user_socket):
         while 1:
             msg = user_socket.receive()
-            if msg == "SEND_JSON":
-                jf = open("./json_file.json")
-                jsonStr = jsonify(json.load(jf)) # load json data
-                user_socket.send(jsonStr)
-        return "123"
+           # if msg == "SEND_JSON":
+               # jf = open("./json_file.json")
+               # jsonStr = jsonify(json.load(jf)) # load json data
+               # user_socket.send(json.load(jf))
+            user_socket.send("server received: " + msg)
+    return "123"
     # else:
     #     pass
 
 if __name__ == '__main__':
     # app.run(debug=False, host='0.0.0.0', port=80)
-    http_serv = WSGIServer(("0.0.0.0", 9999), app, handler_class=WebSocketHandler)
+    http_serv = WSGIServer(("0.0.0.0", 5678), app, handler_class=WebSocketHandler)
     http_serv.serve_forever()
 
 
