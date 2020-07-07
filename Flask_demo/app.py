056abfa68de7ccd775214a70f575a53252cc21c4
ShadowChris <cai.zi.mo@163.com>
2020-07-07 12:06:29 +0800

    任务明确与细化

--- Flask_demo/app.py
+++ Flask_demo/app.py
@@ -3,7 +3,9 @@ from geventwebsocket.handler import WebSocketHandler
 from geventwebsocket.websocket import WebSocket
 from gevent.pywsgi import WSGIServer
 
-app=Flask(__name__)
+app = Flask(__name__)
+client_list = []
+
 
 def check_permit(user_socket):
     msg = user_socket.receive()
@@ -15,35 +17,36 @@ def check_permit(user_socket):
     else:
         response_str = "sorry, the username or password is wrong, please submit again"
         user_socket.send(response_str)
+        return False
 
 
 @app.route("/ws", methods=['GET', 'POST'])
 def ws():
     user_socket = request.environ.get('wsgi.websocket')  # type:WebSocket
-    print("client connected!")
-    jf = open("./json_file.json")
-    jsonStr = json.dumps(json.load(jf)) # load json data
-    print(jsonStr)
-    user_socket.send(jsonStr)
+    # 每接入一个客户端就会加进列表中
+    client_list.append(user_socket)
+    print("client " + str(len(client_list)) + "connected!")
+    # jf = open("./json_file.json")
+    # jsonStr = json.dumps(json.load(jf)) # load json data
+    # print(jsonStr)
+    # user_socket.send(jsonStr)
 
-   # if check_permit(user_socket):
-   #     while 1:
-   #         msg = user_socket.receive()
-           # if msg == "SEND_JSON":
-               # jf = open("./json_file.json")
-               # jsonStr = jsonify(json.load(jf)) # load json data
-               # user_socket.send(json.load(jf))
-           # user_socket.send("server received: " + msg)
+    while 1:
+        if check_permit(user_socket):
+            msg = user_socket.receive()
+            if msg == "SEND_JSON":
+                jf = open("./json_file.json")
+                jsonStr = json.dumps(json.load(jf))  # convert json data to str
+                print(jsonStr)
+                for client in client_list:  # 服务端给多客户端传消息
+                    client.send(jsonStr)
+                    
     return "123"
     # else:
     #     pass
+
 
 if __name__ == '__main__':
     # app.run(debug=False, host='0.0.0.0', port=80)
     http_serv = WSGIServer(("0.0.0.0", 5678), app, handler_class=WebSocketHandler)
     http_serv.serve_forever()
-
-
-
-
-
